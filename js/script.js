b118c0ede4978267ebf956ad0273515f3f6ea96c
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-03 18:14:50 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
-const newClients = ['Peach', 'Houston'];
+function calculateTotal(number) {
+ // Change code below this line
+let sum = 0
+  for (let i = 0; i <= number; i += 1) {
+    sum += i;
+  }
+    return sum;
 
-const allClients = oldClients.concat(newClients);
+  // Change code above this line
+}
+
+console.log(calculateTotal(1))   //1
+console.log(calculateTotal(3))  //6
+console.log(calculateTotal(7))   //28
+console.log(calculateTotal(24))  //300
