5d259990dd5a7cfc91d18d73e9dfa6abc75b41bd
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-04 15:08:51 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
+function findLongestWord(string) {
+  // Change code below this line
+    strArr = string.split(' ');
+    
+    let word = strArr[0];
+    
+    for (let i = 1; i < strArr.length; i += 1) {
 
+        if (word.length < strArr[i].length) {
+            word = strArr[i]
+        } 
 
-function calculateTotalPrice(order) {
-    let total = 0;
-    // Change code below this line
-    for (let i = 0; i < order.length; i += 1) {
-        total += order[i];
+        
+        
     }
-    // Change code above this line
-    console.log(total);
-    return total;
+    return word;
+
+
+  // Change code above this line
 }
 
-
-calculateTotalPrice([412, 371, 94, 63, 176])
+findLongestWord("May the force be with you")
