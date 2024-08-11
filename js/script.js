cbd1a424f0741fc22763bb2d99a459b3b948ab5a
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-11 16:49:07 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
-const colors = [
-  { hex: "#f44336", rgb: "244,67,54" },
-  { hex: "#2196f3", rgb: "33,150,243" },
-  { hex: "#4caf50", rgb: "76,175,80" },
-  { hex: "#ffeb3b", rgb: "255,235,59" },
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
 ];
 
-const hexColors = [];
-const rgbColors = [];
-// Change code below this line
+function getProductPrice(productName) {
+  // Change code below this line
 
-for (const color of colors) {
-  hexColors.push(color.hex);
-  rgbColors.push(color.rgb)
+  for (const product of products) {
+    if (product.name === productName) {
+      return product.price;
+    }
+  } return null;
+    
+  // Change code above this line
 }
 
-console.log(hexColors);
-console.log(rgbColors);
+
+console.log(getProductPrice("Engine")); //возвращает 1200
