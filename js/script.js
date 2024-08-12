81c65a47719c98965c4fa13dc4f68435306e7bb7
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-12 17:50:44 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,10 @@
-const products = [
-  { name: "Radar", price: 1300, quantity: 4 },
-  { name: "Scanner", price: 2700, quantity: 3 },
-  { name: "Droid", price: 400, quantity: 7 },
-  { name: "Grip", price: 1200, quantity: 9 },
-];
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
+// Change code below this line
+const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+const bestScore = Math.max(...allScores);
+const worstScore = Math.min(...allScores);
 
-function getAllPropValues(propName) {
-  // Change code below this line
-  
-  let value = [];
-
-  for (const prop of products) {
-    
-    if (prop.hasOwnProperty(propName)) {
-      value.push(prop[propName])
-    } 
-
-  } return value;
-    // Change code above this line
-    
-}
-
-console.log(getAllPropValues("price")); //возвращает [1300, 2700, 400, 1200]
+console.log(bestScore);
+console.log(worstScore);
