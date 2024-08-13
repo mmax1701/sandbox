083631f2859d3554464d9f9d20dfbb65348ab1c2
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-13 17:50:21 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,14 @@
-const bookShelf = {
-  books: ["The last kingdom", "Haze", "The guardian of dreams"],
-  updateBook(oldName, newName) {
+const atTheOldToad = {
+  potions: ["Speed potion", "Dragon breath", "Stone skin"],
+  addPotion(potionName) {
     // Change code below this line
 
-    let idx = this.books.indexOf(oldName)
-    
-
-return this.books.splice(idx, 1, newName)
+    this.potions.push(potionName);
 
     // Change code above this line
   },
 };
 
-console.log(bookShelf);
-// Дополни метод updateBook(oldName, newName) так, чтобы он изменял название книги с oldName на newName в свойстве books.
-// Используй indexOf() для того, чтобы найти нужный элемент массива, и splice() для того чтобы заменить этот элемент
+console.log(atTheOldToad);
 
-bookShelf.updateBook("Haze", "Dungeon chronicles"); //значение свойства books это массив ["The last kingdom", "Dungeon chronicles", "The guardian of dreams"]
+atTheOldToad.addPotion("Invisibility") //в свойстве potions будет массив ["Speed potion", "Dragon breath", "Stone skin", "Invisibility"]
