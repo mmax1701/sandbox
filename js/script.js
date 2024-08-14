a1c72d558729c782aef2879a27e11efc73424e8d
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-14 18:21:43 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,19 +10,37 @@ getPotions() {
   },
 
   addPotion(newPotion) {
-    for (let potion of this.potions) {
-      if (potion === newPotion) {
-      return `Error! Potion ${newPotion} is already in your inventory!`;
+    for (i = 0; i <= this.potions.length - 1; i += 1) {
+      if (this.potions[i].name === newPotion.name) {
+        return `Error! Potion ${this.potions[i].name} is already in your inventory!`;
+      }
     }
-  } this.potions.push(newPotion);
+    this.potions.push(newPotion);
   },
   
   removePotion(potionName) {
-    
+    for (i = 0; i <= this.potions.length-1; i += 1) {
+      if (this.potions[i].name === potionName) {
+        this.potions.splice(i, 1);
+        break;
+      }
+    }  return `Potion ${potionName} is not in inventory!`;
   },
+
+  updatePotionName(oldName, newName) {
+    for (i = 0; i <= this.potions.length-1; i += 1) {
+      if (this.potions[i].name === oldName) {
+        this.potions[i].name = newName;
+        break;
+      }
+    }  return `Potion ${oldName} is not in inventory!`;
+  }
 };
 
 
 
+
+
 console.log(atTheOldToad);
-atTheOldToad.removePotion("Dragon breath")
+
+atTheOldToad.addPotion({ name: 'Stone skin', price: 240 })
