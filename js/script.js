deb1ade23c8bdc1eadcfc27f4b5309e4338de3f3
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-14 18:45:48 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,14 @@
-const atTheOldToad = {
-  potions: [
-    { name: "Speed potion", price: 460 },
-    { name: "Dragon breath", price: 780 },
-    { name: "Stone skin", price: 520 },
-  ],
-  // Change code below this line
-getPotions() {
-    return this.potions;
-  },
+function registerGuest(name, callback) {
+  console.log(`Реєструємо гостя ${name}.`);
+  callback(name);
+}
 
-  addPotion(newPotion) {
-    for (i = 0; i <= this.potions.length - 1; i += 1) {
-      if (this.potions[i].name === newPotion.name) {
-        return `Error! Potion ${this.potions[i].name} is already in your inventory!`;
-      }
-    }
-    this.potions.push(newPotion);
-  },
-  
-  removePotion(potionName) {
-    for (i = 0; i <= this.potions.length-1; i += 1) {
-      if (this.potions[i].name === potionName) {
-        this.potions.splice(i, 1);
-        break;
-      }
-    }  return `Potion ${potionName} is not in inventory!`;
-  },
+// Передаємо інлайн функцію greet у якості колбека
+registerGuest("Манго", function greet(name) {
+  console.log(`Ласкаво просимо ${name}.`);
+});
 
-  updatePotionName(oldName, newName) {
-    for (i = 0; i <= this.potions.length-1; i += 1) {
-      if (this.potions[i].name === oldName) {
-        this.potions[i].name = newName;
-        break;
-      }
-    }  return `Potion ${oldName} is not in inventory!`;
-  }
-};
-
-
-
-
-
-console.log(atTheOldToad);
-
-atTheOldToad.addPotion({ name: 'Stone skin', price: 240 })
+// Передаємо інлайн функцію notify у якості колбека
+registerGuest("Полі", function notify(name) {
+  console.log(`Шановний(а) ${name}, ваш номер буде готовий за 30 хвилин.`);
+});
