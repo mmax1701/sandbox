c969579f051d326121555b09802b1db313cc4630
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-20 10:19:15 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-function makePizza() {
-  return "Your pizza is being prepared, please wait.";
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
 }
-// Change code below this line
 
-const result = makePizza();
-const pointer = makePizza;
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
 
-console.log(makePizza);
+// Chande code below this line
+function makeMessage(pizzaName, callback) {
+  callback(pizzaName);
+  return;
+}
+
