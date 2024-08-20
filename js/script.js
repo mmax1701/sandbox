5c3298aea4e7a6a84c41dd71ea7406293c957d15
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-20 09:44:05 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-const numbers = [5, 10, 15, 20, 25];
-
-// Класичний for
-for (let i = 0; i < numbers.length; i += 1) {
-  console.log(`Індекс ${i}, значення ${numbers[i]}`);
+function makePizza() {
+  return "Your pizza is being prepared, please wait.";
 }
+// Change code below this line
 
-// Метод перебирання forEach
-numbers.forEach(function (number, index) {
-  console.log(`Індекс ${index}, значення ${number}`);
-});
+const result = makePizza();
+const pointer = makePizza;
+
+console.log(makePizza);
