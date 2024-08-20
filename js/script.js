74732d49f3d5429b736964998aec8997248e0bba
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-20 22:04:06 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
-function deliverPizza(pizzaName) {
-  return `Delivering ${pizzaName} pizza.`;
-}
+const pizzaPalace = {
+  pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+  order(pizzaName, onSuccess, onError) {
+    for (let i = 0; i <= this.pizzas.length - 1; i++) {
+      if (pizzaName === this.pizzas[i]) {
+       return onSuccess(pizzaName)
+      } 
+    }
+   return onError(`There is no pizza with a name ${pizzaName} in the assortment.`)
+  },
+};
+// Change code above this line
 
+// Callback for onSuccess
 function makePizza(pizzaName) {
-  return `Pizza ${pizzaName} is being prepared, please wait...`;
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
 }
 
-// Chande code below this line
-function makeMessage(pizzaName, callback) {
-  callback(pizzaName);
-  return;
+// Callback for onError
+function onOrderError(error) {
+  return `Error! ${error}`;
 }
 
+// Method calls with callbacks
+pizzaPalace.order('Smoked', makePizza, onOrderError);
+pizzaPalace.order('Four meats', makePizza, onOrderError);
+pizzaPalace.order('Big Mike', makePizza, onOrderError);
+pizzaPalace.order('Vienna', makePizza, onOrderError);
+
+
+console.log(pizzaPalace.order("Smoked", makePizza, onOrderError)); // "Your order is accepted. Cooking pizza Smoked."
+console.log(pizzaPalace.order("Vienna", makePizza, onOrderError)); //"Error! There is no pizza with a name Vienna in the assortment."
