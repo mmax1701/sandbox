0d6209847e17c9116d50e8675517d809b7225ebf
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-21 21:01:58 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
-function changeEven(numbers, value) {
-  // Change code below this line
-  const arr = [...numbers]
-  for (let i = 0; i < arr.length; i += 1) {
-    if (arr[i] % 2 === 0) {
-      arr[i] = arr[i] + value;
-    }
-  } return console.log(arr);
-  // Change code above this line
-}
+const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+// Change code below this line
+const planetsLengths = planets.map(planet => planet.length);
 
-changeEven([2, 8, 3, 7, 4, 6], 10) //возвращает новый массив [12, 18, 3, 7, 14, 16]
+console.log(planetsLengths);
