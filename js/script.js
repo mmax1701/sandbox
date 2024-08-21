314ed43398ff96ce626818f8c84b270c6e5c6c77
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-21 21:37:08 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    genres: ["adventure", "history"],
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    genres: ["fiction", "mysticism"],
+  },
+  {
+    title: "Redder Than Blood",
+    author: "Tanith Lee",
+    genres: ["horror", "mysticism", "adventure"],
+  },
+];
 // Change code below this line
-const planetsLengths = planets.map(planet => planet.length);
-
-console.log(planetsLengths);
+const allGenres = books.flatMap(book => book.genres);
+console.log(allGenres);
+const uniqueGenres = allGenres.filter((item, idx, arr) => arr.indexOf(item) === idx );
+console.log(uniqueGenres);
