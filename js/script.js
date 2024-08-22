d575306bb2e6146359b4e6ce5e578c95bede12f8
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-22 09:47:25 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,3 @@
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
-  },
-  {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
-  },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
-];
-
-const MIN_RATING = 8;
-const AUTHOR = "Bernard Cornwell";
 // Change code below this line
-
-const topRatedBooks = books.filter(book => book.rating >= MIN_RATING);
-const booksByAuthor = books.filter(({author}) => author === AUTHOR);
+const getUsersWithEyeColor = (users, color) => {return users.filter(user => user.eyeColor === color)};
+// Change code above this line
