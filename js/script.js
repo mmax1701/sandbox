9c4a84f0a8a6350d6e6bc3b641b1aaff837b1e53
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-23 09:33:53 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 const firstArray = [26, 94, 36, 18];
 const secondArray = [17, 61, 23];
 const thirdArray = [17, 26, 94, 61, 36, 23, 18];
-// Change code below this line
+// Change below this line
 
-const eachElementInFirstIsEven = firstArray.every(item => item % 2 === 0);
-const eachElementInFirstIsOdd = firstArray.every(item => item % 2 !== 0);
+const anyElementInFirstIsEven = firstArray.some(item => item % 2 === 0);
+const anyElementInFirstIsOdd = firstArray.some(item => item % 2 !== 0);
 
-const eachElementInSecondIsEven = secondArray.every(item => item % 2 === 0);
-const eachElementInSecondIsOdd = secondArray.every(item => item % 2 !== 0);
+const anyElementInSecondIsEven = secondArray.some(item => item % 2 === 0);
+const anyElementInSecondIsOdd = secondArray.some(item => item % 2 !== 0);
 
-const eachElementInThirdIsEven = thirdArray.every(item => item % 2 === 0);
-const eachElementInThirdIsOdd = thirdArray.every(item => item % 2 !== 0);
+const anyElementInThirdIsEven = thirdArray.some(item => item % 2 === 0);
+const anyElementInThirdIsOdd = thirdArray.some(item => item % 2 !== 0);
