d5caf74af86a71b35375c86dd858b5ee40e8bf54
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-24 12:00:31 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
-const players = {
-  mango: 1270,
-  poly: 468,
-  ajax: 710,
-  kiwi: 244
-};
-const playtimes = Object.values(players); // [1270, 468, 710, 244]
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+];
 // Change code below this line
 
-const totalPlayTime = playtimes.reduce((total, val) => total + val, 0);
-console.log(totalPlayTime);
+const sortedByAuthorName = [...books].sort((a, b) => a.author.localeCompare(b.author));
 
-// Change code above this line
-const averagePlayTime = totalPlayTime / playtimes.length;
+const sortedByReversedAuthorName = [...books].sort((a, b) => b.author.localeCompare(a.author));
+
+const sortedByAscendingRating = [...books].sort((a, b) => a.rating - b.rating);
+
+const sortedByDescentingRating = [...books].sort((a, b) => b.rating - a.rating);
