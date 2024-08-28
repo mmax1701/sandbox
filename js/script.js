9e5ef6d668f89dec8340566983aa1c91511a7949
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-28 19:53:36 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,13 @@
-const historyService = {
-  orders: [
-    { email: "jacob@hotmail.com", dish: "Burrito" },
-    { email: "solomon@topmail.net", dish: "Burger" },
-    { email: "artemis@coldmail.net", dish: "Pizza" },
-    { email: "solomon@topmail.net", dish: "Apple pie" },
-    { email: "jacob@hotmail.com", dish: "Taco" },
-  ],
-  // Change code below this line
-  getOrdersLog() {
-    return this.orders
-      .map(order => `email: ${order.email} dish: ${order.dish}`)
-      .join(" - ");
-  },
-  getEmails() {
-    const emails = this.orders.map(order => order.email);
-    const uniqueEmails = new Set(emails);
-    return [...uniqueEmails];
-  },
-  getOrdersByEmail(email) {
-    return this.orders.filter(order => order.email === email);
-  },
-  // Change code above this line
+const parent = {
+  name: "Stacey",
+  surname: "Moore",
+  age: 54,
+  heritage: "Irish",
 };
+// Change code below this line
 
+const child = Object.create(parent);
 
-console.log(historyService.getEmails()); //возвращает [{ email: "solomon@topmail.net", dish: "Burger" }, { email: "solomon@topmail.net", dish: "Apple pie" }]
+// Change code above this line
+child.name = "Jason";
+child.age = 27;
