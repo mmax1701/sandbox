30572c3452d376b8781238b34248bb39645ef2eb
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-28 19:34:30 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-// Change code below this line
-const getTotalBalanceByGender = (users, gender) => [...users].filter(user => user.gender === gender).reduce((total, user) => total + user.balance, 0);
-// Change code above this line
+const historyService = {
+  orders: [
+    { email: "jacob@hotmail.com", dish: "Burrito" },
+    { email: "solomon@topmail.net", dish: "Burger" },
+    { email: "artemis@coldmail.net", dish: "Pizza" },
+    { email: "solomon@topmail.net", dish: "Apple pie" },
+    { email: "jacob@hotmail.com", dish: "Taco" },
+  ],
+  // Change code below this line
+  getOrdersLog() {
+    return this.orders
+      .map(order => `email: ${order.email} dish: ${order.dish}`)
+      .join(" - ");
+  },
+  getEmails() {
+    const emails = this.orders.map(order => order.email);
+    const uniqueEmails = new Set(emails);
+    return [...uniqueEmails];
+  },
+  getOrdersByEmail(email) {
+    return this.orders.filter(order => order.email === email);
+  },
+  // Change code above this line
+};
+
+
+console.log(historyService.getEmails()); //возвращает [{ email: "solomon@topmail.net", dish: "Burger" }, { email: "solomon@topmail.net", dish: "Apple pie" }]
