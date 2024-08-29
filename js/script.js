75b62bc59bed4ab8dd343ddb9aa744fb3da1518f
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-29 19:34:27 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-const ancestor = {
-  name: "Paul",
-  age: 83,
-  surname: "Dawson",
-  heritage: "Irish",
-};
-// Change code below this line
-
-const parent = Object.create(ancestor);
-parent.name = "Stacey";
-parent.surname = "Moore";
-parent.age = 54;
-
-const child = Object.create(parent);
-child.name = "Jason";
-child.age = 27;
-
-// Change code above this line
+class Car {
+  
+}
