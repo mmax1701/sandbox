b7446eff097799ed442ecb9bf4ee7095966ed519
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-29 21:12:35 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-class Car {
-  
+class Storage {
+    constructor(items) {
+        this.items = items;
+    }
+
+    getItems() {
+        return this.items;
+    }
+
+    addItem(newItem) {
+        this.items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        const findIdx = this.items.indexOf(itemToRemove);
+        this.items.splice(findIdx, 1)
+        }
 }
+
+
+// Change code above this line
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
