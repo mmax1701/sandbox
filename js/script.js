15b0eb05146243e4523b3283052940e75d643703
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-08-31 19:26:21 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,20 @@
-class StringBuilder {
+class Car {
+  static #MAX_PRICE = 50000;
   // Change code below this line
-  #value; 
-  constructor(initialValue) {
-    this.#value = initialValue;
+  static checkPrice(price) {
+    if (price > Car.#MAX_PRICE) {
+      return 'Error! Price exceeds the maximum'
+    }
+    return 'Success! Price is within acceptable limits'
   }
-
-  getValue() {
-    return this.#value;
-  }
-
-  padEnd(str) {
-    this.#value += str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.padStart(str);
-    this.padEnd(str);
+  // Change code above this line
+  constructor({ price }) {
+    this.price = price;
   }
 }
 
-// Change code above this line
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+const audi = new Car({ price: 36000 });
+const bmw = new Car({ price: 64000 });
+
+console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
