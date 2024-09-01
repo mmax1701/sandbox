d47f2819e899a383ed4776883aee24ab8dc12359
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-09-01 13:51:16 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,18 @@
-class Car {
-  static #MAX_PRICE = 50000;
-  // Change code below this line
-  static checkPrice(price) {
-    if (price > Car.#MAX_PRICE) {
-      return 'Error! Price exceeds the maximum'
-    }
-    return 'Success! Price is within acceptable limits'
+class User {
+  constructor(email) {
+    this.email = email;
   }
 
-  static checkPrice(total) {
-    return total;
-   }
-  // Change code above this line
-  constructor({ price, total }) {
-    this.price = price;
-    this.total = total;
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
   }
 }
+// Change code below this line
 
-const audi = new Car({ price: 36000, total: 2 });
-const bmw = new Car({ price: 64000, total: 5 });
-
-console.log(Car.checkPrice(audi.total)); // "Success! Price is within acceptable limits"
-console.log(Car.checkPrice(bmw.total)); // "Error! Price exceeds the maximum"
+class Admin extends User {
+  static AccessLevel = { BASIC: "basic", SUPERUSER: "superuser" }
+}
